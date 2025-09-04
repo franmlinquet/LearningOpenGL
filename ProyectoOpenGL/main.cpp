72135ee9646ec018f8584df3a297b53b3eb1608c
Francisco M Linquet <franmlinquet@gmail.com>
2025-09-04 01:38:20 -0300

    stbi_image.h setted up

--- ProyectoOpenGL/main.cpp
+++ ProyectoOpenGL/main.cpp
@@ -105,6 +105,7 @@ int main() {
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+		stbi_set_flip_vertically_on_load(true);
 		/* Load Image, Create Texture, Generate Mipmap */
 		unsigned char* data = stbi_load("assets/textures/container.jpg", &width, &height, &nrChannels, 0);
 
