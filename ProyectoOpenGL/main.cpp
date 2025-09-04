af4d3ecb1b5f215dce48ca143f8b78e2523df106
Francisco M Linquet <franmlinquet@gmail.com>
2025-09-04 01:51:41 -0300

    Small modifications

--- ProyectoOpenGL/main.cpp
+++ ProyectoOpenGL/main.cpp
@@ -39,13 +39,6 @@ int main() {
 	Shader myShader("vertexShaderSource.glsl", "fragmentShaderSource.glsl");
 
 
-	/* Vertex data[] */
-	/*float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // Bottom left
-		0.5f, -0.5f, 0.0f,  // Bottom right
-		0.0f, 0.5f, 0.0f    // Top mid
-	};*/
-
 	float vertices[] = {
 		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
 		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
