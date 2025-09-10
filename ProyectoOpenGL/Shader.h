af24f1822a95b4bfbd8f7b3c747d0afc99291d86
Francisco M Linquet <franmlinquet@gmail.com>
2025-09-10 01:53:52 -0300

    GLM included to project

--- ProyectoOpenGL/Shader.h
+++ ProyectoOpenGL/Shader.h
@@ -87,7 +87,6 @@ public:
 		glUseProgram(ID);
 	}
 
-
 	// Utility (uniform functions)
 	void setBool(const std::string &name, bool value) const
 	{ 
--- ProyectoOpenGL/main.cpp
+++ ProyectoOpenGL/main.cpp
@@ -1,4 +1,7 @@
 #include "libs.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "Shader.h"
 
 
