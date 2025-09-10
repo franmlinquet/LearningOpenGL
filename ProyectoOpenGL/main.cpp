a9c93eb6b6c0b27a3f39f4054d77fa8a4c090253
Francisco M Linquet <franmlinquet@gmail.com>
2025-09-10 02:08:09 -0300

    Transform >> translate & rotation over the square Shader

--- ProyectoOpenGL/main.cpp
+++ ProyectoOpenGL/main.cpp
@@ -157,11 +157,17 @@ int main() {
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		
+		/* Transform */
+		glm::mat4 transform = glm::mat4(1.0f);
+		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
-		/* Drawing Triangle */
+		/* Drawing */
 		myShader.use();
+		unsigned int transformLoc = glGetUniformLocation(myShader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
 		glBindVertexArray(VAO);
-		
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		/* Swap Buffer */
