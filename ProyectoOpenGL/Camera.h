065c6796650520fd84b1e9127798e11fff28909f
Francisco M Linquet <franmlinquet@gmail.com>
2025-09-22 18:57:18 -0300

    Constructors implemented & updateCameraVectors function created

--- ProyectoOpenGL/Camera.h
+++ ProyectoOpenGL/Camera.h
@@ -21,7 +21,47 @@ float ZOOM = 45.0f;
 
 class Camera
 {
+public:
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
 
+	float Yaw;
+	float Pitch;
+
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = position;
+		WorldUp = up;
+		Yaw = yaw;
+		Pitch = pitch;
+
+		updateCameraVectors();
+	}
+
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = glm::vec3(posX, posY, posZ);
+		WorldUp = glm::vec3(upX, upY, upZ);
+		Yaw = yaw;
+		Pitch = pitch;
+
+		updateCameraVectors();
+	}
+
+
+
+private:
+	void updateCameraVectors()
+	{
+
+	}
 };
 
 #endif // !CAMERA_H
