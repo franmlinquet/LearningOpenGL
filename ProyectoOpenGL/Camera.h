db8c9e042eef88277cd1840c1e4bff645b27c4ed
Francisco M Linquet <franmlinquet@gmail.com>
2025-09-22 18:48:22 -0300

    CAMERA CLASS, ENUM_MOVEMENTS & PRE-SETTING

--- ProyectoOpenGL/Camera.h
+++ ProyectoOpenGL/Camera.h
@@ -5,7 +5,24 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+enum Camera_Movement
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
 
+float YAW = -90.0f;
+float PITCH = 0.0f;
+float SPEED = 2.5f;
+float SENSITIVITY = 0.1;
+float ZOOM = 45.0f;
+
+class Camera
+{
+
+};
 
 #endif // !CAMERA_H
 
